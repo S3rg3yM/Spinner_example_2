e33756571a881634b8133cf0e4382b9606b29b0b
S3rg3yM <S3rg3y@i.ua>
2016-12-02 13:25:43 +0200

    add onItemSelected Color

--- app/src/main/java/com/example/admin/spinner_example_2/MainActivity.java
+++ app/src/main/java/com/example/admin/spinner_example_2/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.admin.spinner_example_2;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -40,8 +41,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             models.add(new Model(R.drawable.ic_spinner,"Model " + i));
         }
 
-        adapter = new MyAdapter(this,android.R.layout.simple_spinner_item,models);
-
+        adapter = new MyAdapter(this, models);
         adapter.notifyDataSetChanged();
 
         spinner.setAdapter(adapter);
@@ -55,7 +55,9 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         Object item = parent.getItemAtPosition(position);
         Log.d(TAG, "Spinner position: " + item);
-
+        boolean status = models.get(position).state;
+        models.get(position).state = !status;
+        adapter.notifyDataSetChanged();
     }
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
@@ -68,10 +70,11 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         private ArrayList<Model> aModel;
         LayoutInflater lInflater;
 
-        public MyAdapter(Context context, int textViewResourceId, ArrayList<Model> aModel) {
+        public MyAdapter(Context context, ArrayList<Model> aModel) {
 
             this.context = context;
-            this.aModel = aModel;
+            this.aModel = new ArrayList<>(aModel);
+//            this.aModel = new ArrayList<>(aModel);  для создания спиннера на основе нвоо листа
             lInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         }
 
@@ -101,7 +104,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
             ((TextView) view.findViewById(R.id.tv_spinner)).setText(m.result);
             ((ImageView) view.findViewById(R.id.icon_spinner)).setImageResource(m.image);
-
+            view.setBackgroundColor(m.state ? Color.YELLOW : Color.TRANSPARENT);
             return  view;
         }
     }
